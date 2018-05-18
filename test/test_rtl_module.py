214487f89ac63c78a7e85c2e4848de21089dc510
prya <padraigryan@hotmail.com>
2018-05-18 18:03:10 +0100

    last commit before delete

--- test/test_rtl_module.py
+++ test/test_rtl_module.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python
 
 import env
-from module import *
+from module import Module
 import unittest
 
 
 class ModuleTest(unittest.TestCase):
 
-    def testSimpleHier(self):
-        top_mod = Module()
-        low_mod = Module()
-        addr_mod = Module()
 
-        top_mod.parse_rtl_file("../test/samples/rtl/sample.v")
-        low_mod.parse_rtl_file("../test/samples/rtl/sample2.v")
-        # addr_mod.parse_rtl_file("../test/samples/rtl/adder.vhd")
-        top_mod.sub_blocks.append(low_mod)
-        # top_mod.sub_blocks.append(addr_mod)
-
-        top_mod.export_rtl("my_sample.v")
 
 
 if __name__ == "__main__":
